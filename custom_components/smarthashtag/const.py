638089ae37f86c3c5922ae685cb7f0520336ff0a
Bastian Neumann <neumann.bastian@gmail.com>
2024-08-10 19:28:54 +0000

    bump version to 0.4.3

--- custom_components/smarthashtag/const.py
+++ custom_components/smarthashtag/const.py
@@ -9,7 +9,7 @@ LOGGER: Logger = getLogger(__package__)
 NAME = "Smart"
 DOMAIN = "smarthashtag"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "0.4.2"
+VERSION = "0.4.3"
 
 ATTRIBUTION = "Data provided by http://smart.com/"
 ISSUE_URL = "https://github.com/DasBasti/SmartHashtag/issues"
