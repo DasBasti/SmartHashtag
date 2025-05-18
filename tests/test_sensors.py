51084257e1c3675013abd2001bc6cef50c081070
Bastian Neumann <neumann.bastian@gmail.com>
2025-05-18 09:02:55 +0000

    tests: add tests for charging update

--- tests/test_sensors.py
+++ tests/test_sensors.py
@@ -174,3 +174,138 @@ async def test_battery_updates(hass: HomeAssistant, smart_fixture: respx.Router)
 
     assert state
     assert state.state == "44"
+
+
+@pytest.mark.asyncio()
+async def test_charging_ac_power_updates(
+    hass: HomeAssistant, smart_fixture: respx.Router
+):
+    """
+    Test the charging power calculation function
+
+    This loads sample data from pysmarthashtag but changed charging values.
+    """
+    value_target = (0, 0)
+
+    async def deplete_battery(request: Request, route: respx.Route) -> Response:
+        response = load_response(RESPONSE_DIR / "vehicle_info.json")
+        # pysmarthashtag ChargingState['CHARGING']
+        response["data"]["vehicleStatus"]["additionalVehicleStatus"][
+            "electricVehicleStatus"
+        ]["chargerState"] = "2"
+        response["data"]["vehicleStatus"]["additionalVehicleStatus"][
+            "electricVehicleStatus"
+        ]["chargeUAct"] = str(float(value_target[0]))
+        response["data"]["vehicleStatus"]["additionalVehicleStatus"][
+            "electricVehicleStatus"
+        ]["chargeIAct"] = str(float(value_target[1]))
+
+        return Response(200, json=response)
+
+    smart_fixture.get(
+        "https://api.ecloudeu.com/remote-control/vehicle/status/TestVIN0000000001?latest=True&target=basic%2Cmore&userId=112233",
+    ).mock(side_effect=deplete_battery)
+
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        data={
+            "username": "sample_user",
+            "password": "sample_password",
+            "vehicle": "TestVIN0000000001",
+        },
+    )
+
+    entry.add_to_hass(hass)
+
+    await hass.config_entries.async_setup(entry.entry_id)
+    await hass.async_block_till_done()
+
+    _ = hass.states.async_all()
+
+    value_target = (230, 2)
+    await entry.runtime_data.async_refresh()
+
+    state = hass.states.get("sensor.smart_charging_status")
+    assert state
+    assert state.state == "charging"
+
+    state = hass.states.get("sensor.smart_charging_power")
+    assert state
+    assert state.state == "460.0"
+
+    value_target = (230, 3)
+    await entry.runtime_data.async_refresh()
+
+    state = hass.states.get("sensor.smart_charging_power")
+    assert state
+    assert state.state == "690.0"
+
+    # test 3-phase 11kW (11085.1251684408W)
+    value_target = (400, 16)
+    await entry.runtime_data.async_refresh()
+
+    state = hass.states.get("sensor.smart_charging_power")
+    assert state
+    assert state.state == "11085.1251684408"
+
+
+@pytest.mark.asyncio()
+async def test_charging_dc_power_updates(
+    hass: HomeAssistant, smart_fixture: respx.Router
+):
+    """
+    Test the charging power calculation function
+
+    This loads sample data from pysmarthashtag but changed charging.
+    """
+    value_target = 0
+
+    async def deplete_battery(request: Request, route: respx.Route) -> Response:
+        response = load_response(RESPONSE_DIR / "vehicle_info.json")
+        # pysmarthashtag ChargingState['CHARGING']
+        response["data"]["vehicleStatus"]["additionalVehicleStatus"][
+            "electricVehicleStatus"
+        ]["chargerState"] = "15"
+        response["data"]["vehicleStatus"]["additionalVehicleStatus"][
+            "electricVehicleStatus"
+        ]["dcChargeIAct"] = str(float(value_target))
+
+        return Response(200, json=response)
+
+    smart_fixture.get(
+        "https://api.ecloudeu.com/remote-control/vehicle/status/TestVIN0000000001?latest=True&target=basic%2Cmore&userId=112233",
+    ).mock(side_effect=deplete_battery)
+
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        data={
+            "username": "sample_user",
+            "password": "sample_password",
+            "vehicle": "TestVIN0000000001",
+        },
+    )
+
+    entry.add_to_hass(hass)
+
+    await hass.config_entries.async_setup(entry.entry_id)
+    await hass.async_block_till_done()
+
+    _ = hass.states.async_all()
+
+    value_target = 100
+    await entry.runtime_data.async_refresh()
+
+    state = hass.states.get("sensor.smart_charging_status")
+    assert state
+    assert state.state == "dc_charging"
+
+    state = hass.states.get("sensor.smart_charging_power")
+    assert state
+    assert state.state == "41700"
+
+    value_target = 150
+    await entry.runtime_data.async_refresh()
+
+    state = hass.states.get("sensor.smart_charging_power")
+    assert state
+    assert state.state == "62550"
